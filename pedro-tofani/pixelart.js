c05a84f4eb9f424e26d011c3c812fd5fcc8d856a
phtf92 <phtf92@gmail.com>
2019-11-27 15:32:40 -0300

    função onload e refiz o grid

--- pedro-tofani/pixelart.js
+++ pedro-tofani/pixelart.js
@@ -1,13 +1,16 @@
-let color = 'black'
-for (let i = 0; i < 4; i++) {
-    let ponteiroCor = document.getElementsByClassName('corSelecionada')[i];
-    ponteiroCor.addEventListener('click', function () {
-        for (let j = 0; j < 4; j++){
-            let zerarBorda = document.getElementsByClassName('corSelecionada')[j];
-            zerarBorda.style.border = 'white solid';
-        }
-        ponteiroCor.style.border = 'red solid 3px';
-        color = ponteiroCor.id;
-    })
-}
 
+let color = 'black'
+for (let i = 0window.onload = function () {
+    ; i < 4; i++) {
+        let ponteiroCor = document.getElementsByClassName('corSelecionada')[i];
+        ponteiroCor.addEventListener('click', function () {
+            for (let j = 0; j < 4; j++) {
+                let zerarBorda = document.getElementsByClassName('corSelecionada')[j];
+                zerarBorda.style.border = 'white solid';
+            }
+            ponteiroCor.style.border = 'red solid 3px';
+            color = ponteiroCor.id;
+        })
+    }
+
+}
