7bfd27d46c218568769a7cc2dd09f1289b2f2cd1
phtf92 <phtf92@gmail.com>
2019-11-27 15:37:20 -0300

    Arrumei onload function

--- pedro-tofani/pixelart.js
+++ pedro-tofani/pixelart.js
@@ -1,16 +1,16 @@
 
+window.onload = function () {
+}
 let color = 'black'
-for (let i = 0window.onload = function () {
-    ; i < 4; i++) {
-        let ponteiroCor = document.getElementsByClassName('corSelecionada')[i];
-        ponteiroCor.addEventListener('click', function () {
-            for (let j = 0; j < 4; j++) {
-                let zerarBorda = document.getElementsByClassName('corSelecionada')[j];
-                zerarBorda.style.border = 'white solid';
-            }
-            ponteiroCor.style.border = 'red solid 3px';
-            color = ponteiroCor.id;
-        })
-    }
+for (let i = 0; i < 4; i++) {
+    let ponteiroCor = document.getElementsByClassName('corSelecionada')[i];
+    ponteiroCor.addEventListener('click', function () {
+        for (let j = 0; j < 4; j++) {
+            let zerarBorda = document.getElementsByClassName('corSelecionada')[j];
+            zerarBorda.style.border = 'white solid';
+        }
+        ponteiroCor.style.border = 'red solid 3px';
+        color = ponteiroCor.id;
+    })
 
 }
