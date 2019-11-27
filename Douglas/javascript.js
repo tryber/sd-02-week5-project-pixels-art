376365d78978e3731bb6c9519668859247fd1bb3
Douglas Henrique <doughrqe@gmail.com>
2019-11-27 19:53:18 -0300

    Inicio do ultimo bonus

--- Douglas/javascript.js
+++
@@ -1,5 +0,0 @@
-const localClicado= document.querySelectorAll('.tabela');
-localClicado.addEventListener('mousedown',function(event){
-  console.log(event.target);
-});
-console.log('linkado')
--- Douglas/script.js
+++ Douglas/script.js
@@ -2,11 +2,40 @@
 /*global someFunction event:true*/
 const escolha = document.getElementById('cor');
 const localClicado = document.querySelector ('.colorir');
+const array = document.querySelectorAll ('.quadrado');
+const clear= document.getElementById ('clear');
+let cor = 'black';
+const criaDiv = document.createElement ('div');
+const criaTd = document.createElement ('td');
+const criaTr = document.createElement ('tr');
+const up = document.getElementById ('up');
+const down = document.getElementById ('down');
+const contador = document.getElementById ('contador');
+let valor = 5;
+contador.innerText = valor;
+up.addEventListener ('click' , function(){
+  contador.innerText = (valor +1) ;
+  valor += 1;
+  adicionaQuadrado ();
+})
+
+function adicionaQuadrado(){
+  console.log(contador.innerText)
+  if ( contador.innerText > 50 ){
+    alert ('Valor Invalido');
+    contador.innerText = (valor-1);
+    return valor -= 1;
+  }
+}
+
+clear.addEventListener('click',function(){
+  limpar();
+})
 localClicado.addEventListener ('mouseup', function () {
   event.target.style.backgroundColor=cor;
 });
 const selecionaCor = document.querySelector ('.escolhas');
-let cor = 'black';
+
 
 selecionaCor.addEventListener ('mouseup', function () {
   cor = qualACor();
@@ -26,5 +55,10 @@ function qualACor () {
       return 'pink';
     default:
       return 'black';
+  }
+}
+function limpar(){
+  for( let i = 0; array[i] != undefined; i++) {
+    array[i].style.backgroundColor = 'white'
   }
 }
