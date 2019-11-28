184891cd56d187c04666209d59739b21ccfd7d6e
fernandoluckesi <fernando.luckesi94@gmail.com>
2019-11-27 21:12:37 -0300

    projeto finalizado sem estilização e bonus

--- projetos/arte-com-pixels/script.js
+++ projetos/arte-com-pixels/script.js
@@ -1,16 +1,31 @@
+let caixaPreta = document.getElementById('black');
 let caixaVermelha = document.getElementById('red');
-
+let caixaAzul = document.getElementById('blue');
+let caixaVerde = document.getElementById('green');
 let color;
+
+caixaPreta.addEventListener('click', function(){
+    color = caixaPreta.id;
+})
 
 caixaVermelha.addEventListener('click', function(){
     color = caixaVermelha.id;
-    console.log(color)
 })
 
+caixaAzul.addEventListener('click', function(){
+    color = caixaAzul.id;
+})
 
+caixaVerde.addEventListener('click', function(){
+    color = caixaVerde.id;
+})
 
-let principal = document.getElementById('quadrado-principal');
+let quadradoPrincipal = document.getElementById('quadrado-principal');
 
-principal.addEventListener('click',function(event){
-    event.target.style.backgroundColor = color 
+quadradoPrincipal.addEventListener('click',function(event){
+    if(color == undefined){
+        event.target.style.backgroundColor = 'black'
+    }else{
+        event.target.style.backgroundColor = color 
+    }
 })
