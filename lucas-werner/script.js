c7822792144de2e750b93d5622b47b8b754e1947
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-28 10:59:27 -0300

    entregável com requisitos mínimos e bônus da mudança de cor do quadrado selecionado

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,1 +1,19 @@
-let cor1 = document.getElementsByClassName('quadrado')
+let color = 'black'
+
+//laço para interar a paleta
+for (let i = 0; i < 4; i++) {
+let corSelecionada = document.getElementsByClassName('tinta')[i];
+    corSelecionada.addEventListener('click', function () {
+        for (let j=0; j < 4; j++) { // laço interno para limpar todas as bordas
+            let limparBordas = document.getElementsByClassName('tinta')[j];
+            limparBordas.style.border = 'white solid';
+        }
+        corSelecionada.style.border = 'red solid 3px'; // colorir a que foi selecionada
+        color = corSelecionada.id;
+    })
+}
+
+document.getElementById('grid').addEventListener('click', colorir);
+
+function colorir(event) {
+    event.target.style.backgroundColor = color }
