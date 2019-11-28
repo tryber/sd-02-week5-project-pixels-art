fb8c2adf0a55daaf0a9900066c5e405976e0447f
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2019-11-28 16:51:22 -0200

    Adicionado bonus: Botao de reset

--- JulioCezar/script.js
+++ JulioCezar/script.js
@@ -1,14 +1,17 @@
 window.onload = function () {
-  let colorsDivs = document.getElementsByClassName('div_color_pallet');
-  let div_color1 = document.querySelector('#black');
+  document.querySelector('h1').style.color = 'black';
+  const colorsDivs = document.getElementsByClassName('div_color_pallet');
+  const div_color1 = document.querySelector('#black');
   div_color1.addEventListener("mouseup", mouseUp);
-  let div_color2 = document.getElementById('green');
+  const div_color2 = document.getElementById('green');
   div_color2.addEventListener("mouseup", mouseUp);
-  let div_color3 = document.querySelector('#red');
+  const div_color3 = document.querySelector('#red');
   div_color3.addEventListener("mouseup", mouseUp);
-  let div_color4 = document.getElementById('purple');
+  const div_color4 = document.getElementById('purple');
   div_color4.addEventListener("mouseup", mouseUp);
   document.querySelector('.grid_container').addEventListener("click", pixel);
+  const button_reset =  document.getElementById('button_reset');
+  button_reset.addEventListener('click', reset);
 }
 
 function pixel (event) {  
@@ -19,4 +22,12 @@ function pixel (event) {
 function mouseUp (event) {
   let targetColor = event.target.id;
   document.querySelector('h1').style.color = targetColor;  
+}
+
+function reset () {
+  const pixels = document.querySelectorAll('.pixels');
+  const tamanho = pixels.length;
+  for (let i = 0; i < tamanho; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
 }
