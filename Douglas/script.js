17c208955ffe6358d1c63a01c3782061dcc698cb
Douglas Henrique <doughrqe@gmail.com>
2019-11-28 03:12:51 -0300

    Finalizado com todos os bonus

--- Douglas/script.js
+++ Douglas/script.js
@@ -2,32 +2,80 @@
 /*global someFunction event:true*/
 const escolha = document.getElementById('cor');
 const localClicado = document.querySelector ('.colorir');
-const array = document.querySelectorAll ('.quadrado');
+const tbody= document.getElementsByTagName ('tbody');
 const clear= document.getElementById ('clear');
 let cor = 'black';
-const criaDiv = document.createElement ('div');
-const criaTd = document.createElement ('td');
-const criaTr = document.createElement ('tr');
 const up = document.getElementById ('up');
 const down = document.getElementById ('down');
 const contador = document.getElementById ('contador');
-let valor = 5;
-contador.innerText = valor;
-up.addEventListener ('click' , function(){
-  contador.innerText = (valor +1) ;
-  valor += 1;
-  adicionaQuadrado ();
+
+contador.innerText = 5;
+down.addEventListener('click', function(){
+  if ( parseInt(contador.innerText) > 5  ){
+    contador.innerText= parseInt(contador.innerText) - 1 ;
+    removeLinha();
+    removeColuna();
+  }
+  else{
+    alert ('Valor Invalido');
+  }
 })
 
-function adicionaQuadrado(){
-  console.log(contador.innerText)
-  if ( contador.innerText > 50 ){
+up.addEventListener ('click' , function(){
+  if ( parseInt(contador.innerText) >= 50  ){
     alert ('Valor Invalido');
-    contador.innerText = (valor-1);
-    return valor -= 1;
+  }
+  else{
+  contador.innerText= parseInt(contador.innerText) + 1 ;
+  adicionaLinha();
+  DivETd();
+}
+})
+function removeColuna(){
+  const pega= document.querySelectorAll('.linhas');
+
+  for(let i=0; i < parseInt(contador.innerText); i++){
+  const tira= []
+  tira[i] = pega[i].lastChild;
+  pega[i].removeChild (tira[i]) ;
   }
 }
+function removeLinha(){
+  const ultimo = tbody[1].lastChild;
+  tbody[1].removeChild(ultimo);
+}
 
+function adicionaLinha(){
+  const criaTr = document.createElement ('tr');
+  tbody[1].appendChild (criaTr);
+  criaTr.className= ("class", 'linhas');
+  criaTdDiv();
+}
+
+function criaTdDiv(){
+  const criaDiv = [];
+  const criaTd = [];
+  const pega= document.querySelectorAll('.linhas');
+  for(let i= 0; i < parseInt(contador.innerText -1) ; i++){
+  criaTd[i]= document.createElement ('td');
+  pega[ parseInt(contador.innerText) -1].appendChild (criaTd[i]);
+  criaDiv[i]= document.createElement ('div');
+  criaTd[i].appendChild(criaDiv[i]);
+  criaDiv[i].className= ("class", "quadrado")
+  }
+}
+function DivETd(){
+  const criaTd = [];
+  const criaDiv = [];
+  const trs= document.querySelectorAll ('.linhas')
+  for( i = 0 ; i != parseInt(contador.innerText) ; i++) {
+    criaTd[i]=document.createElement ('td');
+    trs[i].appendChild(criaTd[i]);
+    criaDiv[i]= document.createElement ('div');
+    criaTd[i].appendChild(criaDiv[i]);
+    criaDiv[i].className= ("class", "quadrado")
+  }
+}
 clear.addEventListener('click',function(){
   limpar();
 })
@@ -39,7 +87,7 @@ const selecionaCor = document.querySelector ('.escolhas');
 
 selecionaCor.addEventListener ('mouseup', function () {
   cor = qualACor();
-  escolha.style.backgroundColor=cor;
+  escolha.style.backgroundColor= cor;
 });
 
 function qualACor () {
@@ -53,12 +101,35 @@ function qualACor () {
       return 'yellow';
     case 'rosa':
       return 'pink';
+    case 'verde':
+      return 'green';
+    case 'branco':
+      return 'white';
     default:
       return 'black';
   }
 }
 function limpar(){
-  for( let i = 0; array[i] != undefined; i++) {
+  const array = document.querySelectorAll ('.quadrado');
+  for( let i = 0; array[i] != undefined ; i++) {
     array[i].style.backgroundColor = 'white'
+  }
+}
+const but= document.getElementById('random');
+but.addEventListener('click', function() {
+  const para = setInterval ( function (){
+    limpar()
+    clearInterval(para);
+    random()
+
+  }, 10 );
+
+})
+function random(){
+  const array = document.querySelectorAll ('.quadrado');
+  for( let i = 0 ; array[i] != undefined ; i++){
+    if( Math.floor(Math.random() * 10) < 6){
+      array[Math.floor(Math.random() * (parseInt(contador.innerText) * parseInt(contador.innerText))  )].style.backgroundColor= cor;
+    }
   }
 }
