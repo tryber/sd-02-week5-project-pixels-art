8ed3e24eb55a72adb654803adc1951cb282b8a4f
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-11-29 01:24:25 -0300

    Bug Squash Anterior: 35 faltando

--- johnatas-henrique/js/script.js
+++ johnatas-henrique/js/script.js
@@ -1,4 +1,4 @@
-const containerPaleta = document.querySelector(".grid-paleta");
+const containerPaleta = document.querySelector('.grid-paleta');
 const selecionado = document.querySelector('.selecionado');
 const containerCaixa = document.querySelector('.conjunto-pixels');
 let corAtual = 'black';
@@ -6,22 +6,22 @@ const limpaTudo = document.querySelector('.borracha');
 /* Escolhendo a cor */
 
 function selecionaCor(event) {
-  selecionado.className = 'paleta ' + event.target.classList[1];
+  selecionado.className = `paleta ${event.target.classList[1]}`;
   corAtual = event.target.classList[1];
 }
 
 function paletaLoop(itemPaleta) {
-  itemPaleta.addEventListener("click", selecionaCor)
+  itemPaleta.addEventListener('click', selecionaCor)
 }
 
-containerPaleta.querySelectorAll("div").forEach(paletaLoop);
+containerPaleta.querySelectorAll('div').forEach(paletaLoop);
 
 /* Pintando as caixas */
 
 function pintarCaixa(itemPixels) {
   itemPixels.addEventListener('click', function () {
-    itemPixels.className = 'pixels ' + corAtual;
-  })
+    itemPixels.className = `pixels ${corAtual}`;
+  });
 }
 
 containerCaixa.querySelectorAll('div').forEach(pintarCaixa);
@@ -30,9 +30,12 @@ containerCaixa.querySelectorAll('div').forEach(pintarCaixa);
 
 function limparCaixa(itemPixels) {
   itemPixels.className = 'pixels white';
-  selecionado.className = 'paleta selecionado black'
+  selecionado.className = 'paleta selecionado black';
   corAtual = 'black';
 }
-limpaTudo.addEventListener('click', function () {
+
+function botaoLimpaTudo() {
   containerCaixa.querySelectorAll('div').forEach(limparCaixa);
-})
+}
+
+limpaTudo.addEventListener('click', botaoLimpaTudo);
