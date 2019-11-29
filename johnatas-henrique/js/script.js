2e4590e3aec2dfdd36c14bc607fb4e9ae19f8fcb
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-11-29 00:35:19 -0300

    Alterações no layout e bônus de botão para limpar tudo - Iniciando Bug Squash

--- johnatas-henrique/js/script.js
+++ johnatas-henrique/js/script.js
@@ -2,26 +2,37 @@ const containerPaleta = document.querySelector(".grid-paleta");
 const selecionado = document.querySelector('.selecionado');
 const containerCaixa = document.querySelector('.conjunto-pixels');
 let corAtual = 'black';
-
+const limpaTudo = document.querySelector('.borracha');
 /* Escolhendo a cor */
 
-function selecionaCor(event){
-    selecionado.className = 'paleta ' + event.target.classList[1];
-    corAtual = event.target.classList[1]; 
-} 
+function selecionaCor(event) {
+  selecionado.className = 'paleta ' + event.target.classList[1];
+  corAtual = event.target.classList[1];
+}
 
-function paletaLoop(item) {
-    item.addEventListener("click", selecionaCor)
+function paletaLoop(itemPaleta) {
+  itemPaleta.addEventListener("click", selecionaCor)
 }
 
 containerPaleta.querySelectorAll("div").forEach(paletaLoop);
 
 /* Pintando as caixas */
 
-function pintarCaixa(itempintar){
-    itempintar.addEventListener('click', function(){
-        itempintar.className = 'pixels '+corAtual;
-    })
+function pintarCaixa(itemPixels) {
+  itemPixels.addEventListener('click', function () {
+    itemPixels.className = 'pixels ' + corAtual;
+  })
 }
 
 containerCaixa.querySelectorAll('div').forEach(pintarCaixa);
+
+/* Botão apaga tudo */
+
+function limparCaixa(itemPixels) {
+  itemPixels.className = 'pixels white';
+  selecionado.className = 'paleta selecionado black'
+  corAtual = 'black';
+}
+limpaTudo.addEventListener('click', function () {
+  containerCaixa.querySelectorAll('div').forEach(limparCaixa);
+})
