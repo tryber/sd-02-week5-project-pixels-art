0f0a95e7667d00404874f10597a553bafdc66f20
guicgs <etohlif@protonmail.com>
2019-11-29 14:57:50 -0300

    novas correções codeclimate

--- guicgs/pixelArt.js
+++ guicgs/pixelArt.js
@@ -6,10 +6,10 @@ function cliquePaleta() {
 function cliquePixel() {
   this.style.backgroundColor = corSelecionada;
 }
-for(const element of paletaCor) {
+for (const element of paletaCor) {
   element.addEventListener('click', cliquePaleta);
   const quadradosVazios = document.getElementsByClassName('quadradoLinha');
-  for(const element2 of quadradosVazios) { 
-    element.addEventListener('click', cliquePixel);
+  for (const element2 of quadradosVazios) {
+    element2.addEventListener('click', cliquePixel);
   }
 }
