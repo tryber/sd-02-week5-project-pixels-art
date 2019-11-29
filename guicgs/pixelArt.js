8f38cd3bb402716797fb57b7ff0710bd872d5225
guicgs <etohlif@protonmail.com>
2019-11-29 14:43:34 -0300

    coloquei as funções fora do 'for'

--- guicgs/pixelArt.js
+++ guicgs/pixelArt.js
@@ -1,13 +1,15 @@
 let paletaCor = document.getElementsByClassName("quadradoPaleta");
 let corSelecionada = paletaCor[0].style.backgroundColor;
 for (let element of paletaCor) {
-  element.addEventListener('click', function () {
-    corSelecionada = element.style.backgroundColor;
-  });
+  element.addEventListener('click', cliquePaleta);
   let quadradosVazios = document.getElementsByClassName("quadradoLinha");
   for (let element of quadradosVazios) { 
-    element.addEventListener('click', function () {
-    element.style.backgroundColor = corSelecionada;
-    });
-  };
-};
+    element.addEventListener('click', cliquePixel);
+  }
+}
+function cliquePaleta () {
+  corSelecionada = this.style.backgroundColor;
+}
+function cliquePixel () {
+  this.style.backgroundColor = corSelecionada;
+}
