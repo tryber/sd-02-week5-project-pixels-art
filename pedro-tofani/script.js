4311cdf3e8db04ac8dd2dff2c1dbb15d1e8b6274
phtf92 <phtf92@gmail.com>
2019-12-02 19:03:18 -0300

    funcao símbolo Trybe

--- pedro-tofani/script.js
+++ pedro-tofani/script.js
@@ -36,6 +36,7 @@ document.getElementById('clear').addEventListener('click', clear);
 document.getElementById('tamanho').addEventListener('change', clear);
 document.getElementById('tamanho').addEventListener('change', mudarTamanho);
 document.getElementById('gitHub').addEventListener('click', gitHub);
+document.getElementById('trybe').addEventListener('click', trybe);
 
 
 function colorir(event) {
@@ -83,3 +84,48 @@ function gitHub() {
         }
     }
 }
+
+function trybe() {
+    let totalQuadrados = document.querySelectorAll('.colorivel').length;
+    let totalLinha = Math.sqrt(totalQuadrados);
+
+    for (let j = 0; j < totalLinha; j++) {
+        for (let i = 0; i < totalLinha; i++) {
+            document.getElementsByClassName('colorivel')[i + totalLinha * j].value = i + totalLinha * j;
+            document.getElementsByClassName('colorivel')[i + totalLinha * j].style.backgroundColor = '#0fa36b'
+        }
+    }
+
+    for (let j = 27; j < (totalLinha - 12); j++) {
+        for (let i = 0; i < totalLinha; i++) {
+            if (i < 30 && i > 19) document.getElementsByClassName('colorivel')[i + totalLinha * j].style.backgroundColor = 'black'
+        }
+    }
+    for (let j = 16; j < (totalLinha - 23); j++) {
+        for (let i = 0; i < totalLinha; i++) {
+            document.getElementsByClassName('colorivel')[i + totalLinha * j].style.backgroundColor = 'black'
+        }
+    }
+    for (let j = 16; j < (totalLinha - 23); j++) {
+        for (let i = 0; i < totalLinha; i++) {
+            if (i < (10 + (j - 16))) {
+                document.getElementsByClassName('colorivel')[i + totalLinha * j].style.backgroundColor = '#0fa36b'
+                document.getElementsByClassName('colorivel')[(totalLinha * (j + 1) - 1) - i].style.backgroundColor = '#0fa36b'
+            }
+        }
+    }
+    for (let j = 16; j < 20; j++) {
+        for (let i = 19 + (j - 16); i < 21 + (j - 16); i++) {
+            document.getElementsByClassName('colorivel')[i + totalLinha * j].style.backgroundColor = '#0fa36b'
+            document.getElementsByClassName('colorivel')[(totalLinha * (j + 1) - 1) - i].style.backgroundColor = '#0fa36b'
+        }
+    }
+    for (let j = 20; j < 22; j++) {
+        for (let i = 23 + (j - 20); i < 25; i++) {
+            document.getElementsByClassName('colorivel')[i + totalLinha * j].style.backgroundColor = '#0fa36b'
+            document.getElementsByClassName('colorivel')[(totalLinha * (j + 1) - 1) - i].style.backgroundColor = '#0fa36b'
+        }
+    }
+}
+
+
